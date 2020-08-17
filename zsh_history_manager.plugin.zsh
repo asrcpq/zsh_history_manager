733c6b0c55ac823961339e18c81e536965318595
asrcpq <asrcpq@gmail.com>
2020-08-17 06:16:41 +0000

    more filesystem check

--- zsh_history_manager.plugin.zsh
+++ zsh_history_manager.plugin.zsh
@@ -2,12 +2,20 @@ function zsh_history_check() {
 	local last_time=0
 	local LN=0
 	local check_pass=0
-	cat "$HISTFILE" | while read -r line; do
+	if ! [ -r "$1" ]; then
+		echo "History file not readable!"
+		return 2
+	fi
+	cat "$1" | while read -r line; do
 		LN=$(( $LN + 1 ))
 		if ( echo $line | grep '^: [0-9]*:[0-9]*;' > /dev/null ); then
 			local new_time="$(echo $line | cut -d':' -f2 | tr -d ' ')"
 			if [ "$new_time" -lt "$last_time" ]; then
 				echo "Note: time goes back for $(($last_time - $new_time))s in $LN"
+			fi
+			if [ "$last_time" -eq "0" ]; then
+				echo -n "History start at: "
+				date -d @"$new_time"
 			fi
 			last_time=$new_time
 		else
@@ -19,22 +27,44 @@ function zsh_history_check() {
 			check_pass=1
 		fi
 	done
+	if [ "$check_pass" -ne 1 ]; then
+		echo -n "History end at: "
+		date -d @"$last_time"
+	fi
+	last_time=$new_time
 	return check_pass
 }
 
 function zsh_history_merge() {
-	if ! zsh_history_check; then
-		echo "History check failed, refuse to merge"
+	echo "Local history check"
+	if ! zsh_history_check "$HISTFILE"; then
+		echo "History check failed for local history, refuse to merge"
 		return 1
 	fi
-	if ! [ -w "$1" ]; then
+	echo "Passed\n"
+
+	echo "Remote history check"
+	if ! [ -f "$1" ]; then
+		echo "File not exist, creating"
 		if ! ( touch "$1" ); then
-			echo "Cannot open target file for writing"
+			echo "Cannot create file"
 			return 2
 		else
 			echo "File created"
 		fi
+	else
+		if ! zsh_history_check "$1"; then
+			echo "History check failed for remote history, refuse to merge"
+			return 4
+		fi
 	fi
+	if ! [ -w "$1" ] || ! [ -r "$1" ]; then
+		echo "Cannot open target file for rw"
+		return 3
+	fi
+	echo "Passed\n"
+
+	echo "Merging"
 	echo -n "$(wc -l < "$1")"+"$(wc -l < "$HISTFILE")"=
 	cat "$HISTFILE" >> "$1"
 	sort -uo "$1" "$1"
