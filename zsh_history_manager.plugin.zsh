5af811d886c042c78131ee72fa323ce58320b905
asrcpq <asrcpq@gmail.com>
2020-08-19 02:04:53 +0000

    make all variables local

--- zsh_history_manager.plugin.zsh
+++ zsh_history_manager.plugin.zsh
@@ -16,15 +16,20 @@ zsh_history_merge() {
 	printf "Passed\n\n"
 
 	echo "Generating tmp histfile"
-	tmphist="${XDG_CACHE_HOME-$HOME/.cache}/zsh_history_manager/tmp_history"
+	local tmphist="${XDG_CACHE_HOME-$HOME/.cache}/zsh_history_manager/tmp_history"
 	mkdir -p "$(dirname "$tmphist")"
 	fc -n -t "%s" -l 0 > "$tmphist"
 	printf "Passed\n\n"
 
 	echo "Matching sync-ed history"
+	local LN
 	LN="$(grep -n "^$(head -1 "$tmphist")$" "$1" | head -1 | grep -o '^[0-9]*')"
 	echo "target split: $LN/$(wc -l <"$1")"
-	hist_ln=1 # LN of first new line in tmphist
+	local hist_ln=1 # LN of first new line in tmphist
+	local line_target
+	local line_hist
+	local yn
+	local match_ln
 	if [ -n "$LN" ]; then
 		echo "Find record in $LN, matching remaining"
 		while true; do
@@ -34,19 +39,25 @@ zsh_history_merge() {
 			if [ "$line_hist" != "$line_target" ]; then
 				echo "Line mismatch in target file:$LN tmphist:$hist_ln"
 				echo "Trying to match trailing lines"
-				for ln in {$LN..$(wc -l < "$1")}; do
-					match_ln="$(grep -n "$(sed -nE "${ln}p" "$1")" "$tmphist" | grep -o "^[0-9]*")"
+				local ln=$(( LN - 1 ))
+				trailing_match() {
+					match_ln="$(grep -n "$line_hist" "$tmphist" | grep -o "^[0-9]*" | tr '\n' ',')"
 					if [ -n "$match_ln" ]; then
-						echo "$ln<-->$match_ln"
+						echo "$ln<-->${match_ln%,}"
 					else
 						echo "$ln<-->FAILED..."
 						return 6
 					fi
+					ln=$(( ln + 1 ))
+				}
+				trailing_match || return 6
+				while IFS= read -r line_hist <&3; do
+					trailing_match || return 6
 				done
 				echo "Proceed(y)?"
 				read -r yn
 				[ "$yn" != "y" ] && return 7
-				target_tmp="$XDG_CACHE_HOME/zsh/target_tmp"
+				local target_tmp="$XDG_CACHE_HOME/zsh/target_tmp"
 				head -"$(( LN - 1 ))" "$1" > "$target_tmp"
 				mv "$target_tmp" "$1"
 				break
@@ -61,8 +72,10 @@ zsh_history_merge() {
 	printf "Passed\n\n"
 
 	echo "Joint date verification"
+	local last_target
 	last_target="$(tail -1 "$1" | grep -o "^[0-9]*")"
 	if [ -n "$last_target" ]; then # If saved history exists
+		local first_tmphist
 		first_tmphist="$(sed -nE "${hist_ln}p" "$tmphist" | grep -o "^[0-9]*")"
 		if [ -z "$first_tmphist" ]; then
 			echo "Nothing to append"
